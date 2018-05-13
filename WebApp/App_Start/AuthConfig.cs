e2854e0c512a6b69425e7766397d13d133f23280
Abhishek <abhishekhotte@gmail.com>
2018-05-13 20:38:25 +0530

    Googe oauth authentication, About us -college gallery photos

---
+++ WebApp/App_Start/AuthConfig.cs
@@ -0,0 +1,19 @@
+﻿using DotNetOpenAuth.GoogleOAuth2;
+using Microsoft.AspNet.Membership.OpenAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.App_Start
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            GoogleOAuth2Client clientGoog = new GoogleOAuth2Client("307273604535-ivpr4d1flsp7nobr6nh0ks05a88pm19p.apps.googleusercontent.com", "00BS-VWewOuds2ueo2BKjOAI");
+            IDictionary<string, string> extraData = new Dictionary<string, string>();
+            OpenAuth.AuthenticationClients.Add("google", () => clientGoog, extraData);
+        }
+    }
+}
--- WebApp/Controllers/AboutusController.cs
+++ WebApp/Controllers/AboutusController.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -20,35 +21,37 @@ namespace WebApp.Controllers
         }
 
 
+        #region Staff details
         public string GetStaffListHtml()
         {
             var list = staffDALObj.GetStaffList();
             string profileHtml = "";
             for (int i = 0; i < list.Count; i++)
-                profileHtml += "<div id='staff" + list[i].Id + "' class='col-lg-3 col-md-4 col-sm-6 col-xs-12 wow fadeInUp filter-teaching-staff'><div class='panel panel-default'><div class='panel-body profile'>"+(true? "<i class='fa fa-edit staff-edit-icon' data-staff-id='"+list[i].Id+"' title='Edit'></i>" : "")+ "<div class='profile-image'><img id='liSImg" + list[i].Id + "' src='/images/staff/" + list[i].Id + ".jpg' alt=''></div></div><div class='panel-body'><div class='contact-info'><div class='profile-data-name' id='liSName" + list[i].Id + "'>" + list[i].Name + "</div><div class='profile-data-title' id='liSDesignation" + list[i].Id + "'>" + list[i].Designation + "</div><div class='profile-data-title'><span class='fa-phone fa fa-lg profile-icon'></span><span id='liSMobile" + list[i].Id + "'>" + list[i].Mobile + "</span></div><div class='profile-data-title'><span class='fa-envelope-o fa fa-lg profile-icon'></span><span id='liSEmail" + list[i].Id + "'>" + list[i].Email + "</span></div></div></div></div></div>";
+                profileHtml += "<div id='staff" + list[i].Id + "' class='col-lg-3 col-md-4 col-sm-6 col-xs-12 wow zoomIn fadeInUp filter-teaching-staff'><div class='panel panel-default'><div class='panel-body profile'>" + (User.Identity.IsAuthenticated ? "<i class='fa fa-edit staff-edit-icon' data-staff-id='" + list[i].Id + "' title='Edit'></i>" : "") + "<div class='profile-image'><img id='liSImg" + list[i].Id + "' src='/images/staff/" + list[i].Id + ".jpg' alt=''></div></div><div class='panel-body'><div class='contact-info'><div class='profile-data-name' id='liSName" + list[i].Id + "'>" + list[i].Name + "</div><div class='profile-data-title' id='liSDesignation" + list[i].Id + "'>" + list[i].Designation + "</div><div class='profile-data-title'><span class='fa-phone fa fa-lg profile-icon'></span><span id='liSMobile" + list[i].Id + "'>" + list[i].Mobile + "</span></div><div class='profile-data-title'><span class='fa-envelope-o fa fa-lg profile-icon'></span><span id='liSEmail" + list[i].Id + "'>" + list[i].Email + "</span></div></div></div></div></div>";
             return profileHtml;
 
         }
+
         public ActionResult GetAllStaffDetails()
         {
             try
             {
-                return Json(new { result= "success",list=GetStaffListHtml() ,JsonRequestBehavior.AllowGet});
+                return Json(new { result = "success", list = GetStaffListHtml(), JsonRequestBehavior.AllowGet });
             }
             catch (Exception ex)
             {
-               
+
                 return Json(new { result = "error" }, JsonRequestBehavior.AllowGet);
             }
         }
 
-
+        [CustomAuthorize]
         public ActionResult AddUpdateStaff(StaffDetails s, HttpPostedFileBase file)
         {
             try
             {
-                int staffId=0;
-                var data= staffDALObj.AddUpdate(s, ref staffId);
+                int staffId = 0;
+                var data = staffDALObj.AddUpdate(s, ref staffId);
                 JsonSerializer j = new JsonSerializer();
                 string newImage = Server.MapPath("~/images/staff/" + staffId + ".jpg");
                 if (System.IO.File.Exists(newImage))
@@ -58,8 +61,8 @@ namespace WebApp.Controllers
                 {
 
                     string x = string.Empty;
-                    if(s.Image.Contains("data:image/png;base64,"))
-                        x =  s.Image.Replace("data:image/png;base64,", "");
+                    if (s.Image.Contains("data:image/png;base64,"))
+                        x = s.Image.Replace("data:image/png;base64,", "");
                     else if (s.Image.Contains("data:image/jpg;base64,"))
                         x = s.Image.Replace("data:image/jpg;base64,", "");
                     else
@@ -78,14 +81,80 @@ namespace WebApp.Controllers
                 {
                     System.IO.File.Copy(Server.MapPath("~/images/no-image.jpg"), Server.MapPath("~/images/staff/" + staffId + ".jpg"));
                 }
-                return Json(new { result = "success", staffId = staffId },JsonRequestBehavior.AllowGet);
+                return Json(new { result = "success", staffId = staffId }, JsonRequestBehavior.AllowGet);
             }
             catch (Exception ex)
             {
                 //WriteLogs.Write(ex);
-                return Json(new { result = "error"}, JsonRequestBehavior.AllowGet);
+                return Json(new { result = "error" }, JsonRequestBehavior.AllowGet);
             }
 
+        } 
+        #endregion
+
+        #region Gallery section
+        [CustomAuthorize]
+        [HttpPost]
+        public ActionResult UploadCollegePhotos()
+        {
+            try
+            {
+                HttpFileCollectionBase files = Request.Files;
+                string path = Server.MapPath("~/images/college");
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                for (int i = 0; i < files.Count; i++)
+                {
+                    HttpPostedFileBase file = files[i];
+                    var InputFileName = Path.GetFileName(file.FileName);
+                    var ServerSavePath = Path.Combine(path + "\\" + InputFileName);
+
+                    //Save file to server folder  
+
+                    int count = 1;
+                    checkIfFileExists:
+                    if (System.IO.File.Exists(ServerSavePath))
+                    {
+                        ServerSavePath = Path.Combine(path + "\\" + InputFileName.Substring(0, InputFileName.LastIndexOf('.')) + count++ + InputFileName.Substring(InputFileName.IndexOf('.')));
+                        goto checkIfFileExists;
+                    }
+                    else
+                    {
+                        file.SaveAs(ServerSavePath);
+                    }
+
+                }
+                return Json(new { result = "success" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                WriteLogs.Write(ex);
+                return Json("error", JsonRequestBehavior.AllowGet);
+
+            }
         }
+
+        public ActionResult GetCollegePhotos()
+        {
+            try
+            {
+                string tmpImages = "";
+                DirectoryInfo di = new DirectoryInfo(Server.MapPath("~/images/college"));
+                FileSystemInfo[] files = di.GetFileSystemInfos();
+                files.OrderBy(x => x.LastWriteTime).ToList();
+                foreach (var item in files)
+                {
+                    var img = new FileInfo(item.FullName);
+                    tmpImages += "<div class='col-lg-3 col-md-6 col-md-4 wow zoomIn fadeInUp' data-animation-delay='0s' style='animation-delay: 0s;'> <a class='lightbox' href='" + Url.Content(String.Format("~/images/college/{0}", img.Name)) + "'><img src = '" + Url.Content(String.Format("~/images/college/{0}", img.Name)) + "' /></a></div>";
+                }
+                return Json(new { result = "success", images = tmpImages }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                WriteLogs.Write(ex);
+                return Json("error", JsonRequestBehavior.AllowGet);
+            }
+        }
+        #endregion
     }
 }
